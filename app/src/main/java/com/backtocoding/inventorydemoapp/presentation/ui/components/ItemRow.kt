edf19abd59f369bcc837504b703ad299475c05eb
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-08-25 17:13:50 +0530

    Added: coil dependency, and ItemRow and ItemListScreen

--- app/src/main/java/com/backtocoding/inventorydemoapp/presentation/ui/components/ItemRow.kt
+++ app/src/main/java/com/backtocoding/inventorydemoapp/presentation/ui/components/ItemRow.kt
@@ -1,4 +1,54 @@
 package com.backtocoding.inventorydemoapp.presentation.ui.components
 
-class ItemRow {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.backtocoding.inventorydemoapp.domain.entities.ItemEntity
+
+@Composable
+fun ItemRow(item: ItemEntity, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(8.dp)
+            .background(Color.LightGray)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = item.imageUrl),
+            contentDescription = "Item image for ${item.itemName}",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(Color.Gray)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = item.itemName, fontWeight = FontWeight.Bold)
+        Text(text = "${item.itemPrice}")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewItemRow() {
+    val item = ItemEntity(
+        itemName = "CLh8FsEbezR3Ol",
+        itemPrice = 262,
+        itemBarcode = "615684372",
+        imageUrl = "https://db.ezobooks.in/kappa/image/get/9370740235/f3643e12-1854-4131-9781-f27e39300f7e6336/b2e4c1eb-18c9-43ec-8802-94ef7c764598"
+    )
+    ItemRow(item = item)
 }
--- app/src/main/java/com/backtocoding/inventorydemoapp/presentation/ui/screens/ItemListScreen.kt
+++ app/src/main/java/com/backtocoding/inventorydemoapp/presentation/ui/screens/ItemListScreen.kt
@@ -1,4 +1,51 @@
 package com.backtocoding.inventorydemoapp.presentation.ui.screens
 
-class ItemListScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.backtocoding.inventorydemoapp.presentation.ui.components.ItemRow
+import com.backtocoding.inventorydemoapp.presentation.viewmodel.ItemViewModel
+
+@Composable
+fun ItemListScreen(viewModel: ItemViewModel) {
+    val items by viewModel.items.collectAsState()
+    val loading by viewModel.loading.collectAsState()
+    val error by viewModel.error.collectAsState()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        } else if (error != null) {
+            Text(text = error!!, modifier = Modifier.align(Alignment.Center))
+        } else {
+            LazyColumn {
+                itemsIndexed(items.chunked(2)) { _, rowItems ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        rowItems.forEach { item ->
+                            ItemRow(item = item)
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
